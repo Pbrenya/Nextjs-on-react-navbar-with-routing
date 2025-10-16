5f21f020f5803f97698d5caf896e67118ec3e2be
Pbrenya <brenya1211@gmail.com>
2025-10-16 05:01:03 -0700

    turbopack config added to next.config.ts

--- HeroTemplate/my-app/next.config.ts
+++ HeroTemplate/my-app/next.config.ts
@@ -2,7 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-   
+   turbopack: {
+    root: __dirname, // tells Next.js this is the root
+  },
 };
 
 export default nextConfig;
