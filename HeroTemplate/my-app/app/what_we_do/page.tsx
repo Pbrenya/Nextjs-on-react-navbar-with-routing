e8da4af837fdc7636248dc247e5cd17d1db664ad
Pbrenya <brenya1211@gmail.com>
2025-10-16 13:12:41 -0700

    Lots of componeents added

--- HeroTemplate/my-app/app/what_we_do/page.tsx
+++ HeroTemplate/my-app/app/what_we_do/page.tsx
@@ -1,4 +1,11 @@
 import { HeroHeader } from "@/components/header";
+import { SparklesPreview } from "@/components/Sparklesbelow";
+import { StatsGrid } from "@/components/stats1";
+import { StatsCards } from "@/components/stats2";
+import { FaqsGrid } from "@/components/ui/Faqgrid";
+import { FaqsAccordion } from "@/components/ui/FAQs";
+import { FooterComplex } from "@/components/ui/footer";
+import { SparklesCore } from "@/components/ui/sparkles";
 
 
 
@@ -7,8 +14,20 @@ export default function WhatWeDo() {
 
 
     return(
-
+        <>
         <HeroHeader />
+        <SparklesPreview />
+        <StatsCards />
+        <StatsGrid />
+        <FooterComplex />
+
+        <FaqsAccordion />
+        <FaqsGrid />
+       
+
+
+
+        </>
     );
 }
 
